b921bfe3d1cc60c69674db703be6036be5713822
Codeclan Student <student@Codeclans-MacBook-Pro.local>
2021-05-14 12:51:23 +0100

    guest and song tests ok

--- tests/song_test.py
+++ tests/song_test.py
@@ -8,3 +8,9 @@ class TestSong(unittest.TestCase):
 
     def test_song_has_name(self):
         self.assertEqual("I will survive", self.song.name)
+
+    def test_song_has_genre(self):
+        self.assertEqual("pop", self.song.genre)
+
+    def test_song_has_price(self):
+        self.assertEqual(2, self.song.price)
